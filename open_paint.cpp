9503c922287984b90d7e7479f588dcfffb958f40
Margarita Villarreal <maggsvisan@users.noreply.github.com>
2017-07-18 17:01:09 +0200

    Update open_paint.cpp

--- open_paint.cpp
+++ open_paint.cpp
@@ -6,7 +6,12 @@
 
 using namespace std;
 
-double spine; // 3
+double x_spine;
+double y_spine;
+
+int newX;
+int newY;
+//double spine; // 3
 double y_leftHand;//7
 double y_rightHand;//11
 double x_leftHand;
@@ -36,59 +41,70 @@ void closePaint (){
 void VRPN_CALLBACK handle_tracker( void* userData, const vrpn_TRACKERCB b )
 {
 	if (b.sensor == 3) {
-		spine= b.pos[1];
-		cout << "spine:" <<spine<<endl;
+		x_spine = b.pos[0];
+		y_spine = b.pos[1];
+		//cout << "spine:" <<spine<<endl;
 	}
 
 	if (b.sensor == 0) {
 		y_head= b.pos[1];
-		cout << "y_head:" <<y_head<<endl;
+		x_head= b.pos[0];
+		//cout << "y_head:" <<y_head<<endl;
 	}
 
 	if (b.sensor == 7) {
 		y_leftHand= b.pos[1];
 		x_leftHand=b.pos[0];
-		cout << "y_leftHand:" << y_leftHand<<endl;
+	//	cout << "y_leftHand:" << y_leftHand<<endl;
 	}
 
-	if (b.sensor == 11) {
+	if (b.sensor == 11 && state==1 ) {
 
 		y_rightHand= b.pos[1];
 		x_rightHand=b.pos[0];
-		
+		int newX = (x_rightHand - x_head + 0.3)*1960 ;
+		int newY = -(y_rightHand- y_head)*1020;
 		//move the hand with calibration
-		x_POS= x_rightHand*1000;
-		y_POS= y_rightHand*1000;
+		//x_POS= x_rightHand*30000;
+		//y_POS= -(y_rightHand*2000);
 		
-		SetCursorPos(x_POS, y_POS);
+		//SetCursorPos(x_POS, y_POS);
+		SetCursorPos(newX, newY);
 		
 	}
 
 	result= abs(x_leftHand-x_rightHand);
+	
+
+	cout << "result : "<< result <<"\n";
 
 	//Motion State
-	if (result < 0.04 ) { 
-		cout<< "clap";
+	if (result < 0.4 ) { 
+	//	cout<< "clap";
 
 		if (state==0) {
 			openPaint();
 		}
 	}
 
-	if (y_head < y_leftHand  ){
-		cout << "y_leftHandUp" <<endl;
+	if (y_head < y_leftHand && y_head < y_rightHand ){
+	//	cout << "y_leftHandUp" <<endl;
 		if (state==1){
-			closePaint ();
-		}
-		
+			closePaint();
+		}	
 	}
 
-	else if (spine <y_rightHand){
-		cout<< "y_rightHandUp"<< endl;
+	if ((y_head < y_leftHand) && state==1 ){
+		mouse_event(MOUSEEVENTF_LEFTDOWN,0,0,0,0);
 	}
-
-	else{
-		cout <<"reading"<< endl;
+	
+	if ((y_head > y_leftHand) && state==1 ){
+		mouse_event(MOUSEEVENTF_LEFTUP,0,0,0,0);
+	}
+	
+	
+	if (y_spine < y_rightHand ){
+	//	cout<< "y_rightHandUp"<< endl;
 	}
 }
 
